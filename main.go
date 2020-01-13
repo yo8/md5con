88d3cb919e4b45597cf8400ff9f0dcbd027a52a8
yo8 <timixr@pm.me>
2020-01-14 01:00:09 +0800

    Pseudo random for high perf

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/binary"
 	"fmt"
+	"math/rand"
 	"os"
+	"runtime"
 	"runtime/debug"
 	"strconv"
 	"time"
@@ -15,15 +18,26 @@ import (
 
 const (
 	numOfWorkers           = 32
-	defaultExperimentTimes = 500000
-	minBatchSize           = 10000
-	gcBatchSize            = 100000
+	numOfRandSource        = 8
+	defaultExperimentTimes = 2000000
+	minBatchSize           = 200000
+	gcBatchSize            = 1000000
 	bufferChanSize         = 100000
+	seedChanSize           = numOfWorkers * numOfRandSource * 256
+	refreshRandSourcePer   = 10000
+	saveFullFeature        = false
 )
 
+type Feature struct {
+	Hash    string
+	Content string
+}
+
 var (
-	hashChan  = make(chan string, bufferChanSize)
-	errorChan chan error
+	hashMap     = map[string]string{}
+	seedChan    = make(chan int64, seedChanSize)
+	featureChan = make(chan *Feature, bufferChanSize)
+	errorChan   chan error
 )
 
 func Float2Bytes(feature []float32) (data []byte, err error) {
@@ -33,28 +47,35 @@ func Float2Bytes(feature []float32) (data []byte, err error) {
 	return
 }
 
-func GetRandomFeatureBytes() (bytes []byte, err error) {
+func GetRandomFeatureBytes(sources []*rand.Rand) (bytes []byte, err error) {
 	const floatCount = 256
 
+	numOfSource := len(sources)
+	if numOfSource == 0 {
+		err = fmt.Errorf("got no random source")
+		return
+	}
+
+	source := sources[0]
 	numbers := make([]float32, 0, floatCount)
 	for idx := 0; idx < floatCount; idx++ {
-		var num float32
-		if num, err = yrand.Float32(); err != nil {
-			return
+		numbers = append(numbers, source.Float32())
+		if idx%16 == 0 {
+			source = sources[source.Int()%numOfSource]
 		}
-		numbers = append(numbers, num)
 	}
 
 	bytes, err = Float2Bytes(numbers)
 	return
 }
 
-func GetRandomFeatureHash() (hash string, err error) {
+func GetRandomFeature(sources []*rand.Rand) (fea *Feature, err error) {
 	var bytes []byte
-	if bytes, err = GetRandomFeatureBytes(); err != nil {
+	if bytes, err = GetRandomFeatureBytes(sources); err != nil {
 		return
 	}
 
+	var hash string
 	if hash, err = yhash.BytesMD5(bytes); err == nil {
 		if len(hash) >= 18 {
 			hash = hash[:18]
@@ -63,25 +84,60 @@ func GetRandomFeatureHash() (hash string, err error) {
 		}
 	}
 
+	fea = &Feature{
+		Hash:    hash,
+		Content: "",
+	}
+	if saveFullFeature {
+		fea.Content = base64.StdEncoding.EncodeToString(bytes)
+	}
+
 	return
 }
 
-func StartRandomFeatureHashGenerator(idx int) {
-	var (
-		hash string
-		err  error
-	)
-
-	fmt.Printf("generator #%d starts\n", idx)
+func StartRandomSourceSeedGenerator() {
+	fmt.Println("seeder starts")
 	for {
-		if hash, err = GetRandomFeatureHash(); err == nil {
-			hashChan <- hash
+		if num, err := yrand.Int64Range(-9223372036854775808, 9223372036854775800); err == nil {
+			seedChan <- num
 		} else {
 			errorChan <- err
 			break
 		}
 	}
-	fmt.Printf("generator #%d exits\n", idx)
+	fmt.Println("seeder ends")
+}
+
+func StartRandomFeatureHashGenerator(id int) {
+	var (
+		feature *Feature
+		err     error
+	)
+
+	fmt.Printf("generator #%d starts\n", id)
+
+	count := 0
+	sources := make([]*rand.Rand, 0, numOfRandSource)
+	for {
+		if count == 0 {
+			for idx := 0; idx < numOfRandSource; idx++ {
+				sources = append(sources, rand.New(rand.NewSource(<-seedChan)))
+			}
+		}
+		count++
+
+		if feature, err = GetRandomFeature(sources); err == nil {
+			featureChan <- feature
+		} else {
+			errorChan <- err
+			break
+		}
+
+		if count >= refreshRandSourcePer {
+			count = 0
+		}
+	}
+	fmt.Printf("generator #%d exits\n", id)
 }
 
 func main() {
@@ -104,21 +160,24 @@ func main() {
 		batchSize = minBatchSize
 	}
 
+	go StartRandomSourceSeedGenerator()
 	for idx := 1; idx <= numOfWorkers; idx++ {
 		go StartRandomFeatureHashGenerator(idx)
 	}
 
-	hashMap := map[string]bool{}
-
 	var (
-		hash string
+		hash *Feature
 		err  error
 	)
 
 	startTime := time.Now()
 	for idx := 1; idx <= times; idx++ {
 		select {
-		case hash = <-hashChan:
+		case hash = <-featureChan:
+			if hash == nil {
+				fmt.Printf("index #%d - got null feature: %v\n", idx, hash)
+				continue
+			}
 		case err = <-errorChan:
 			if err != nil {
 				fmt.Printf("index #%d - got error: %v\n", idx, err)
@@ -126,19 +185,35 @@ func main() {
 			}
 		}
 
-		if _, ok := hashMap[hash]; ok {
-			fmt.Printf("index #%d - got conflict: %s\n", idx, hash)
+		if content, ok := hashMap[hash.Hash]; ok {
+			same := content == hash.Content
+			fmt.Printf("index #%d - got conflict: %s, saved feature: %v, exact same: %v\n", idx, hash.Hash, saveFullFeature, same)
+			if saveFullFeature && !same {
+				fmt.Println("1st:", content)
+				fmt.Println("2nd:", hash.Content)
+			}
 			os.Exit(2)
 		}
 
-		hashMap[hash] = true
+		hashMap[hash.Hash] = hash.Content
 
 		if idx%gcBatchSize == 0 {
-			fmt.Printf("done: %.2f%% (%d) - %s √ GC\n", float64(idx)/float64(times)*100, idx, hash)
+			var memBefore, memAfter runtime.MemStats
+			runtime.ReadMemStats(&memBefore)
 			// runtime.GC()
 			debug.FreeOSMemory()
+			runtime.ReadMemStats(&memAfter)
+
+			// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+			fmt.Printf("done: %.2f%% (%d) - GC #%d, Sys: %.2f MiB -> %.2f MiB, Alloc: %.2f MiB -> %.2f MiB, Total: %.2f MiB -> %.2f MiB\n",
+				float64(idx)/float64(times)*100, idx,
+				memAfter.NumGC,
+				float64(memBefore.Sys)/1024/1024, float64(memAfter.Sys)/1024/1024,
+				float64(memBefore.Alloc)/1024/1024, float64(memAfter.Alloc)/1024/1024,
+				float64(memBefore.TotalAlloc)/1024/1024, float64(memAfter.TotalAlloc)/1024/1024,
+			)
 		} else if idx%minBatchSize == 0 {
-			fmt.Printf("done: %.2f%% (%d) - %s - %.2f/sec\n", float64(idx)/float64(times)*100, idx, hash, float64(minBatchSize)/(time.Since(startTime).Seconds()))
+			fmt.Printf("done: %.2f%% (%d) - %s - %.2f/sec\n", float64(idx)/float64(times)*100, idx, hash.Hash, float64(minBatchSize)/(time.Since(startTime).Seconds()))
 			startTime = time.Now()
 		}
 	}
